3c5205f22dc974bcd7585d7754782dee30dfb27b
Shawn Robin <robin.sh@northeastern.edu>
2022-01-24 17:32:55 -0500

    set static folder and URL

--- api/api.py
+++ api/api.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from constants import GENRES
 from utils import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='build', static_url_path='')
 
 @app.route('/get_genres')
 def get_genres():
